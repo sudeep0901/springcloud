a6564e1a2ad597529ebad2406fd7344a1d8aba81
sudeep patel <sudeep.tech.patel@gmail.com>
2018-11-20 16:48:30 +0530

    OAuth git hub working

---
+++ spring-clould-secure-ui-oauth/src/main/java/com/sudeep/secureui/ReportController.java
@@ -0,0 +1,31 @@
+package com.sudeep.secureui;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableOAuth2Sso
+public class ReportController extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/")
+	public String loadHome() {
+
+		return "home";
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http
+		.authorizeRequests()
+		.antMatchers("/", "login**")
+		.permitAll()
+		.anyRequest()
+		.authenticated();
+
+	}
+}
---
+++ spring-clould-secure-ui-oauth/src/main/java/com/sudeep/secureui/SpringClouldSecureUiOauthApplication.java
@@ -0,0 +1,12 @@
+package com.sudeep.secureui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClouldSecureUiOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringClouldSecureUiOauthApplication.class, args);
+	}
+}
