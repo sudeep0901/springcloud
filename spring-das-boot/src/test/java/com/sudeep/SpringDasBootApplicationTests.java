e43152bf3ad11d96722b4868516a2a2846adca81
sudeep patel <sudeep.tech.patel@gmail.com>
2018-11-21 15:58:05 +0530

    Junit Test case successfully executed

--- spring-das-boot/src/test/java/com/sudeep/SpringDasBootApplicationTests.java
+++ spring-das-boot/src/test/java/com/sudeep/SpringDasBootApplicationTests.java
@@ -1,9 +1,13 @@
 package com.sudeep;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sudeep.controller.HomeController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -11,6 +15,11 @@ public class SpringDasBootApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		HomeController hc = new HomeController();
+
+		String result = hc.home();
+
+		assertEquals(result, "Hello Boot");
 	}
 
 }
